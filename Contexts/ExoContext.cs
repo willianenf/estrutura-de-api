022488497f1ded312dd6b5f3f6cb97cc25a76507
Williane <williane010201@gmail.com>
2023-07-09 19:00:29 -0300

    update restante do CRUD

--- Contexts/ExoContext.cs
+++ Contexts/ExoContext.cs
@@ -2,12 +2,13 @@ using Exo.WebApi.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Data.SqlClient;
 using Microsoft.Data.SqlClient;
+
 namespace Exo.WebApi.Contexts
 {
     public class ExoContext : DbContext
-{
+    {
         public ExoContext()
-        {
+        {     
         }
         public ExoContext(DbContextOptions<ExoContext> options) : base(options)
         {
@@ -16,13 +17,16 @@ namespace Exo.WebApi.Contexts
         {
             if(!optionsBuilder.IsConfigured)
             {
-                // Essa string de conexão depende da SUA máquina.
-                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;" + "Database=ExoApi;Trusted_Connection=True;");
-                // Exemplo 1 de string de conexão:
-                // UserID=sa;Password=admin;Server=localhost;Database=ExoApi;- + Trusted_Connection=False;
-            
-                // Exemplo 2 de string de conexão:
-                //Server=localhost\\SQLEXPRESS;Database=ExoApi;Trusted_Connection=True;
+              // Essa string de conexão foi depende da SUA máquina.
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS01;"               
+                              + "Database=ExoApi;Trusted_Connection=True;");
+
+              // Exemplo 1 de string de conexão:
+              // User ID=sa;Password=admin;Server=localhost;Database=ExoApi;-
+              // + Trusted_Connection=False;
+
+              // Exemplo 2 de string de conexão:
+              // Server=localhost\\SQLEXPRESS;Database=ExoApi;Trusted_Connection=True;
             }
         }
         public DbSet<Projeto> Projetos { get; set; }
--- Repositories/ProjetoRepository.cs
+++ Repositories/ProjetoRepository.cs
@@ -4,18 +4,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 namespace Exo.WebApi.Repositories
 {
-public class ProjetoRepository
-{
-private readonly ExoContext _context;
-public ProjetoRepository(ExoContext context)
-{
-_context = context;
-}
-public List<Projeto> Listar()
-{
-return _context.Projetos.ToList();
-}
-}
+    public class ProjetoRepository
+    {
+        private readonly ExoContext _context;
+        public ProjetoRepository(ExoContext context)
+        {
+            _context = context;
+        }
+        public List<Projeto> Listar()
+        {
+            return _context.Projetos.ToList();
+        }
+    }
 }
